fd22fa400d3586c35d8ca931b70502d6c039e9ed
比目鱼(王悦) <bimuyu.wangy@purplechain.cn>
2019-10-22 18:05:05 +0800

    依赖vant

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <van-search
+      v-model="value"
+      placeholder="请输入搜索关键词"
+      show-action
+      shape="round"
+      @search="onSearch"
+    >
+      <div slot="action" @click="onSearch">搜索</div>
+    </van-search>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '哈哈'
+    }
+  },
+
+  methods: {
+    onSearch(event) {
+      console.log(event)
+    }
+  }
+};
+</script>
